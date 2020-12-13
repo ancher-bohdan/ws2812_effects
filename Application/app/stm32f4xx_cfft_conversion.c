d74dfd7585f43d10f83a7dcb0c4355b0ec7dadf9
Bohdan Chupys <chupys.bohdan@gmail.com>
2020-12-13 16:00:22 +0200

    [TASK] Refractoring of cfft coverstion code

--- Application/app/stm32f4xx_cfft_conversion.c
+++ Application/app/stm32f4xx_cfft_conversion.c
@@ -1,33 +1,59 @@
 #define ARM_MATH_CM4
 
 #include "stm32f4xx.h"
-#include "arm_math.h"
+#include "arm_const_structs.h"
 
-float32_t Input[256];
-float32_t Output[128];
+float32_t Input[512];
+float32_t Output[512];
+
+static const arm_cfft_instance_f32 *get_cfft_instanse(int fft_length)
+{
+    const arm_cfft_instance_f32 *S = NULL;
+    
+    switch (fft_length) {
+    case 16:
+      S = &arm_cfft_sR_f32_len16;
+      break;
+    case 32:
+      S = &arm_cfft_sR_f32_len32;
+      break;
+    case 64:
+      S = &arm_cfft_sR_f32_len64;
+      break;
+    case 128:
+      S = &arm_cfft_sR_f32_len128;
+      break;
+    case 256:
+      S = &arm_cfft_sR_f32_len256;
+      break;
+    case 512:
+      S = &arm_cfft_sR_f32_len512;
+      break;
+    case 1024:
+      S = &arm_cfft_sR_f32_len1024;
+      break;
+    case 2048:
+      S = &arm_cfft_sR_f32_len2048;
+      break;
+    case 4096:
+      S = &arm_cfft_sR_f32_len4096;
+      break;
+    }
+
+    return S;
+}
 
 void stm32_cfft_convert(int16_t *buf, uint16_t fft_size)
 {
-    int i = 0;
-    arm_cfft_radix4_instance_f32 cfft;
+    arm_q15_to_float(buf, Input, fft_size << 1);
 
-    arm_cfft_radix4_init_f32(&cfft, fft_size, 0, 1);
-
-    for(i = 0; i < (fft_size << 1); i++)
-    {
-        Input[i] = (float32_t)buf[i];
-    }
-
-    arm_cfft_radix4_f32(&cfft, Input);
+    arm_cfft_f32(get_cfft_instanse(fft_size), Input, 0, 1);
 
     arm_cmplx_mag_f32(Input, Output, fft_size);
 
     buf[0] = 0;
 
-    for(i = 1; i < (fft_size << 1); i++)
-    {
-        buf[i] = (int16_t)Output[i];
-    }
+    arm_float_to_q15(Output, buf, fft_size << 1);
 }
 
 void stm32_normalise_function(int16_t *buf, uint16_t size)
