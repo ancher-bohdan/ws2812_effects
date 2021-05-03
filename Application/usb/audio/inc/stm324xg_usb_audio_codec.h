0f6ebd84ee64c13cca7e9268b54fd3b850f43729
Bohdan Chupys <chupys.bohdan@gmail.com>
2021-05-03 22:45:51 +0300

    [TASK] Add usb audio class. During playing sound has microfreze (~2 ms). Known issue

--- Application/usb/audio/inc/stm324xg_usb_audio_codec.h
+++ Application/usb/audio/inc/stm324xg_usb_audio_codec.h
@@ -1,49 +1,60 @@
 /**
   ******************************************************************************
-  * @file    stm324xg_usb_audio_codec.h
+  * @file    stm32f4_discovery_audio_codec.h
   * @author  MCD Application Team
-  * @version v1.2.1
-  * @date    06-November-2015
-  * @brief   This file contains all the functions prototypes for the audio codec
-  *          low layer driver.
-  *            
-  *     *******************************************************************  
-  *                           IMPORTANT NOTES
-  *     *******************************************************************  
-  *     This file is extracted and modified from the audio codec driver
-  *     "stm324xg_eval_audio_codec.c" provided by STMicroelectronics for
-  *     the STM32F4xx family (in folder Utilities\STM32_EVAL\STM3240_41_G_EVAL).
-  *     This modified driver is intended for use ONLY in the scope of the
-  *     USB Device Audio Example.
-  *        
+  * @version V1.1.0
+  * @date    28-October-2011
+  * @brief   This file contains all the functions prototypes for the 
+  *          stm32f4_discovery_audio_codec.c driver.
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2015 STMicroelectronics.
-  * All rights reserved.</center></h2>
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
   *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                      <http://www.st.com/SLA0044>
-  *
-  ******************************************************************************
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************  
   */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __STM324xG_USB_AUDIOCODEC_H
-#define __STM324xG_USB_AUDIOCODEC_H
+#ifndef __STM32F4_DISCOVERY_AUDIOCODEC_H
+#define __STM32F4_DISCOVERY_AUDIOCODEC_H
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
 
-/* Exported types ------------------------------------------------------------*/
-/* Exported constants --------------------------------------------------------*/
+/** @addtogroup Utilities
+  * @{
+  */
+
+
+/** @addtogroup STM32F4_DISCOVERY
+  * @{
+  */
+    
+/** @defgroup STM32F4_DISCOVERY_AUDIO_CODEC 
+  * @{
+  */    
+
+
+/** @defgroup STM32F4_DISCOVERY_AUDIO_CODEC_Exported_Types
+  * @{
+  */
+
+/** @defgroup STM32F4_DISCOVERY_AUDIO_CODEC_Exported_Constants
+  * @{
+  */ 
 
 /*------------------------------------
              CONFIGURATION: Audio Codec Driver Configuration parameters
-                                      ----------------------------------------*/
-//#define AUDIO_USE_MACROS
+----------------------------------------*/
+/* Audio Transfer mode (I2S Interrupt) */
+//#define I2S_INTERRUPT                 /* Uncomment this line to enable audio transfert with I2S interrupt*/ 
 
 /* Audio Transfer mode (DMA, Interrupt or Polling) */
 #define AUDIO_MAL_MODE_NORMAL         /* Uncomment this line to enable the audio 
@@ -52,35 +63,32 @@
                                          Transfer using DMA */
 
 /* For the DMA modes select the interrupt that will be used */
-/* #define AUDIO_MAL_DMA_IT_TC_EN */  /* Uncomment this line to enable DMA Transfer Complete interrupt */
+#define AUDIO_MAL_DMA_IT_TC_EN        /* Uncomment this line to enable DMA Transfer Complete interrupt */
 /* #define AUDIO_MAL_DMA_IT_HT_EN */  /* Uncomment this line to enable DMA Half Transfer Complete interrupt */
 /* #define AUDIO_MAL_DMA_IT_TE_EN */  /* Uncomment this line to enable DMA Transfer Error interrupt */
-
-/* #define USE_DMA_PAUSE_FEATURE *//* Uncomment this line to enable the use of DMA Pause Feature
-                                 When this define is enabled, the Pause function will
-                                 just pause/resume the DMA without need to reinitialize the
-                                 DMA structure. In this case, the buffer pointer should remain
-                                 available after resume. */
 
 /* Select the interrupt preemption priority and subpriority for the DMA interrupt */
 #define EVAL_AUDIO_IRQ_PREPRIO           0   /* Select the preemption priority level(0 is the highest) */
 #define EVAL_AUDIO_IRQ_SUBRIO            0   /* Select the sub-priority level (0 is the highest) */
 
 /* Uncomment the following line to use the default Codec_TIMEOUT_UserCallback() 
-   function implemented in stm324xg_evel_audio_codec.c file.
+   function implemented in stm32f4_discovery_audio_codec.c file.
    Codec_TIMEOUT_UserCallback() function is called whenever a timeout condition 
    occurs during communication (waiting on an event that doesn't occur, bus 
    errors, busy devices ...). */   
 /* #define USE_DEFAULT_TIMEOUT_CALLBACK */
+
+/* Enable this define to use the I2S DMA for writing into DAC register */
+//#define DAC_USE_I2S_DMA
 /*----------------------------------------------------------------------------*/
 
 /*------------------------------------
                     OPTIONAL Configuration defines parameters
-                                      ----------------------------------------*/
+----------------------------------------*/
 /* I2C clock speed configuration (in Hz) 
   WARNING: 
    Make sure that this define is not already declared in other files (ie. 
-  stm324xg_eval.h file). It can be used in parallel by other modules. */
+  stm322xg_eval.h file). It can be used in parallel by other modules. */
 #ifndef I2C_SPEED
  #define I2C_SPEED                        100000
 #endif /* I2C_SPEED */
@@ -98,18 +106,17 @@
 
 /* Uncomment this line to enable verifying data sent to codec after each write 
   operation */
-/* #define VERIFY_WRITTENDATA */
+#define VERIFY_WRITTENDATA 
 /*----------------------------------------------------------------------------*/
 
 /*-----------------------------------
                     Hardware Configuration defines parameters
-                                     -----------------------------------------*/
-/* I2S peripheral configuration defines */
+-----------------------------------------*/
 /* Audio Reset Pin definition */
 #define AUDIO_RESET_GPIO_CLK           RCC_AHB1Periph_GPIOD  
 #define AUDIO_RESET_PIN                GPIO_Pin_4    
 #define AUDIO_RESET_GPIO               GPIOD 
-
+                 
 /* I2S peripheral configuration defines */
 #define CODEC_I2S                      SPI3
 #define CODEC_I2S_CLK                  RCC_APB1Periph_SPI3
@@ -131,31 +138,51 @@
 #define Audio_I2S_IRQHandler           SPI3_IRQHandler
 
 
- /* I2S DMA Stream definitions */
- #define AUDIO_MAL_DMA_CLOCK            RCC_AHB1Periph_DMA1
- #define AUDIO_MAL_DMA_STREAM           DMA1_Stream7
- #define AUDIO_MAL_DMA_CHANNEL          DMA_Channel_0
- #define AUDIO_MAL_DMA_IRQ              DMA1_Stream7_IRQn
- #define AUDIO_MAL_DMA_FLAG_TC          DMA_FLAG_TCIF7
- #define AUDIO_MAL_DMA_FLAG_HT          DMA_FLAG_HTIF7
- #define AUDIO_MAL_DMA_FLAG_FE          DMA_FLAG_FEIF7
- #define AUDIO_MAL_DMA_FLAG_TE          DMA_FLAG_TEIF7
- #define AUDIO_MAL_DMA_FLAG_DME         DMA_FLAG_DMEIF7
- #define AUDIO_MAL_DMA_FLAG_ALL         (uint32_t)(AUDIO_MAL_DMA_FLAG_TC | AUDIO_MAL_DMA_FLAG_HT |\
-                                                   AUDIO_MAL_DMA_FLAG_FE | AUDIO_MAL_DMA_FLAG_DME)
  #define AUDIO_MAL_DMA_PERIPH_DATA_SIZE DMA_PeripheralDataSize_HalfWord
  #define AUDIO_MAL_DMA_MEM_DATA_SIZE    DMA_MemoryDataSize_HalfWord
-/*#ifdef I2S_24BIT
+#ifdef I2S_24BIT
  #define DMA_MAX_SZE                    (8)
 #else
  #define DMA_MAX_SZE                    (4)
-#endif*/
- #define DMA_MAX_SZE                    (0xFFFF)
- #define AUDIO_MAL_DMA                  DMA1
- #define AUDIO_MAL_DMA_IFCR             HIFCR
+#endif
+
+ #define DAC_DHR12L1_ADDRESS            0x4000740C
+ #define DAC_DHR12R1_ADDRESS            0x40007408
+ #define DAC_DHR8R1_ADDRESS             0x40007410
+ #define AUDIO_DAC_CHANNEL              DAC_Channel_1
+
+ /* I2S DMA Stream definitions */
+ #define AUDIO_I2S_DMA_CLOCK            RCC_AHB1Periph_DMA1
+ #define AUDIO_I2S_DMA_STREAM           DMA1_Stream7
+ #define AUDIO_I2S_DMA_DREG             CODEC_I2S_ADDRESS
+ #define AUDIO_I2S_DMA_CHANNEL          DMA_Channel_0
+ #define AUDIO_I2S_DMA_IRQ              DMA1_Stream7_IRQn
+ #define AUDIO_I2S_DMA_FLAG_TC          DMA_FLAG_TCIF7
+ #define AUDIO_I2S_DMA_FLAG_HT          DMA_FLAG_HTIF7
+ #define AUDIO_I2S_DMA_FLAG_FE          DMA_FLAG_FEIF7
+ #define AUDIO_I2S_DMA_FLAG_TE          DMA_FLAG_TEIF7
+ #define AUDIO_I2S_DMA_FLAG_DME         DMA_FLAG_DMEIF7
+
+ #define Audio_MAL_I2S_IRQHandler       DMA1_Stream7_IRQHandler
 
 
- #define Audio_MAL_IRQHandler           DMA1_Stream7_IRQHandler
+ /* DAC DMA Stream definitions */
+ #define AUDIO_DAC_DMA_CLOCK            RCC_AHB1Periph_DMA1
+ #define AUDIO_DAC_DMA_STREAM           DMA1_Stream0
+ #define AUDIO_DAC_DMA_DREG             DAC_DHR12L1_ADDRESS
+ #define AUDIO_DAC_DMA_CHANNEL          DMA_Channel_0
+ #define AUDIO_DAC_DMA_IRQ              DMA1_Stream0_IRQn
+ #define AUDIO_DAC_DMA_FLAG_TC          DMA_FLAG_TCIF0
+ #define AUDIO_DAC_DMA_FLAG_HT          DMA_FLAG_HTIF0
+ #define AUDIO_DAC_DMA_FLAG_FE          DMA_FLAG_FEIF0
+ #define AUDIO_DAC_DMA_FLAG_TE          DMA_FLAG_TEIF0
+ #define AUDIO_DAC_DMA_FLAG_DME         DMA_FLAG_DMEIF0
+ #define AUDIO_MAL_DMA_FLAG_ALL         (uint32_t)(AUDIO_MAL_DMA_FLAG_TC | AUDIO_MAL_DMA_FLAG_HT |\
+                                                   AUDIO_MAL_DMA_FLAG_FE | AUDIO_MAL_DMA_FLAG_DME)
+
+
+ #define Audio_MAL_DAC_IRQHandler       DMA1_Stream0_IRQHandler
+
 
 /* I2C peripheral configuration defines (control interface of the audio codec) */
 #define CODEC_I2C                      I2C1
@@ -179,7 +206,11 @@
 
 /*-----------------------------------
                         Audio Codec User defines
-                                     -----------------------------------------*/
+-----------------------------------------*/
+/* Audio interface : I2S or DAC */
+#define AUDIO_INTERFACE_I2S           1
+#define AUDIO_INTERFACE_DAC           2
+
 /* Codec output DEVICE */
 #define OUTPUT_DEVICE_SPEAKER         1
 #define OUTPUT_DEVICE_HEADPHONE       2
@@ -202,16 +233,24 @@
 #define AUDIO_MUTE_ON                 1
 #define AUDIO_MUTE_OFF                0
 /*----------------------------------------------------------------------------*/
+/**
+  * @}
+  */ 
 
-/* Exported macro ------------------------------------------------------------*/
+/** @defgroup STM32F4_DISCOVERY_AUDIO_CODEC_Exported_Macros
+  * @{
+  */ 
 #define VOLUME_CONVERT(x)    ((Volume > 100)? 100:((uint8_t)((Volume * 255) / 100)))
 #define DMA_MAX(x)           (((x) <= DMA_MAX_SZE)? (x):DMA_MAX_SZE)
 
+/**
+  * @}
+  */ 
 
-/* Exported functions ------------------------------------------------------- */
-/*-----------------------------------
-                           Generic functions 
-                                    ------------------------------------------*/
+/** @defgroup STM32F4_DISCOVERY_AUDIO_CODEC_Exported_Functions
+  * @{
+  */ 
+void EVAL_AUDIO_SetAudioInterface(uint32_t Interface);
 uint32_t EVAL_AUDIO_Init(uint16_t OutputDevice, uint8_t Volume, uint32_t AudioFreq);
 uint32_t EVAL_AUDIO_DeInit(void);
 uint32_t EVAL_AUDIO_Play(uint16_t* pBuffer, uint32_t Size);
@@ -219,32 +258,13 @@ uint32_t EVAL_AUDIO_PauseResume(uint32_t Cmd, uint32_t Addr, uint32_t Size);
 uint32_t EVAL_AUDIO_Stop(uint32_t CodecPowerDown_Mode);
 uint32_t EVAL_AUDIO_VolumeCtl(uint8_t Volume);
 uint32_t EVAL_AUDIO_Mute(uint32_t Command);
-uint32_t Codec_SwitchOutput(uint8_t Output);
-
-/*-----------------------------------
-                           Audio Codec functions 
-                                    ------------------------------------------*/
-/* High Layer codec functions */
-uint32_t Codec_Init(uint16_t OutputDevice, uint8_t Volume, uint32_t AudioFreq);
-uint32_t Codec_DeInit(void);
-uint32_t Codec_Play(void);
-uint32_t Codec_PauseResume(uint32_t Cmd);
-uint32_t Codec_Stop(uint32_t Cmd);
-uint32_t Codec_VolumeCtrl(uint8_t Volume);
-uint32_t Codec_Mute(uint32_t Cmd);
-void Audio_MAL_IRQHandler(void);
-
-/*-----------------------------------
-                   MAL (Media Access Layer) functions 
-                                    ------------------------------------------*/
-void     Audio_MAL_Init(void);
-void     Audio_MAL_DeInit(void);
-void     Audio_MAL_Play(uint32_t Addr, uint32_t Size);
-void     Audio_MAL_PauseResume(uint32_t Cmd, uint32_t Addr, uint32_t Size);
-void     Audio_MAL_Stop(void);
+void Audio_MAL_Play(uint32_t Addr, uint32_t Size);
+void DAC_Config(void);
 
 /* User Callbacks: user has to implement these functions in his code if
   they are needed. -----------------------------------------------------------*/
+
+#define EVAL_AUDIO_GetSampleCallBack() 0
 
 /* This function is called when the requested data has been completely transferred.
    In Normal mode (when  the define AUDIO_MAL_MODE_NORMAL is enabled) this function
@@ -266,11 +286,28 @@ void EVAL_AUDIO_Error_CallBack(void* pData);
    occurs during communication (waiting on an event that doesn't occur, bus 
    errors, busy devices ...) on the Codec control interface (I2C).
    You can use the default timeout callback implementation by uncommenting the 
-   define USE_DEFAULT_TIMEOUT_CALLBACK in stm324xg_usb_audio_codec.h file.
+   define USE_DEFAULT_TIMEOUT_CALLBACK in stm32f4_discovery_audio_codec.h file.
    Typically the user implementation of this callback should reset I2C peripheral
    and re-initialize communication or in worst case reset all the application. */
 uint32_t Codec_TIMEOUT_UserCallback(void);
- 
-#endif /* __STM324xG_USB_AUDIOCODEC_H */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+#endif /* __STM32F4_DISCOVERY_AUDIOCODEC_H */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */ 
+   
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
